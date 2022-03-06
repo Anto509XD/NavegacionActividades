df102eb8439b94b11bfcd9a3a8c5dd8eaa063707
Anto509XD <75438526+Anto509XD@users.noreply.github.com>
2022-03-06 14:46:45 -0500

    PrimerCommit

---
+++ app/src/main/java/com/example/ejemplonavegacionactividades/ActividadB.kt
@@ -0,0 +1,34 @@
+package com.example.ejemplonavegacionactividades
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class ActividadB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_b)
+
+        val botonC= findViewById<Button>(R.id.botonC)
+        val botonD= findViewById<Button>(R.id.botonD)
+
+        val mensaje= intent.getStringExtra("mensaje")
+
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+
+        botonC.setOnClickListener {
+            var intent= Intent(this, ActividadC::class.java)
+            intent.putExtra("mensaje", "hacia actividad C")
+            intent.putExtra("mensajeA", mensaje)
+            startActivity(intent)
+        }
+
+        botonD.setOnClickListener {
+            var intent= Intent(this, ActividadD::class.java)
+            intent.putExtra("mensaje", "hacia actividad D")
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ejemplonavegacionactividades/ActividadC.kt
@@ -0,0 +1,24 @@
+package com.example.ejemplonavegacionactividades
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class ActividadC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_c)
+
+        val mensaje= intent.getStringExtra("mensaje")
+        val mensajeDeA= intent.getStringExtra("mensajeA")
+        val Boton= findViewById<Button>(R.id.Boton)
+
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+
+        Boton.setOnClickListener {
+            finish()
+        }
+    }
+}
